fce64fa7d668073e50584bbed469eb9a54e2b1a5
BartoszJanowiecki <65231885+BartoszJanowiecki@users.noreply.github.com>
2024-03-23 23:02:19 +0100

    add func average

--- Init/Init/Program.cs
+++ Init/Init/Program.cs
@@ -1,4 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("3");
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] przykladoweLiczby = { 1, 3, 5, 7, 9 };
+        double srednia = ObliczSrednia(przykladoweLiczby);
+        Console.WriteLine($"Średnia: {srednia}");
+    }
+    public static double ObliczSrednia(int[] liczby)
+    {
+        if (liczby == null || liczby.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta.");
+        }
+
+        double suma = 0;
+        foreach (int liczba in liczby)
+        {
+            suma += liczba;
+        }
+
+        return suma / liczby.Length;
+    }
+}
+
+
