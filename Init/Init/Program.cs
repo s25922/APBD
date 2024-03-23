1943946ff690643e5793d5304466d12f23ebd2d9
BartoszJanowiecki <65231885+BartoszJanowiecki@users.noreply.github.com>
2024-03-23 23:28:21 +0100

    modify loopv2

--- Init/Init/Program.cs
+++ Init/Init/Program.cs
@@ -4,11 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] przykladoweLiczby = { 1, 3, 5, 7, 9 };
-        double srednia = ObliczSrednia(przykladoweLiczby);
+        int[] numbers = { 1, 3, 5, 7, 9 };
+        
+        double srednia = ObliczSrednia(numbers);
         Console.WriteLine($"Średnia: {srednia}");
+        
+        int max = FindMaxValue(numbers);
+        Console.WriteLine($"Maksymalna wartość w tablicy to: {max}");
     }
-    public static double ObliczSrednia(int[] liczby)
+    static double ObliczSrednia(int[] liczby)
     {
         if (liczby == null || liczby.Length == 0)
         {
@@ -23,6 +27,22 @@ class Program
 
         return suma / liczby.Length;
     }
+    
+    static int FindMaxValue(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta");
+        }
+x
+        int maxValue = array[0];
+        for (int l = 1; l < array.Length; l++)
+        {
+            if (array[l] > maxValue)
+            {
+                maxValue = array[l];
+            }
+        }
+        return maxValue;
+    }
 }
-
-
